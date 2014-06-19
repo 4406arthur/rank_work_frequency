3876d20ae4f2583b0f381e75b92b0a6c90e81ef3
Joe <mantou820430@gmail.com>
2014-06-19 20:56:25 +0800

    translate feature

---
+++ src/trans_area.py
@@ -0,0 +1,27 @@
+from azure_translate import trans
+
+
+count=0
+container=[]
+chinese=[]
+with open ("voc.data", "r") as myfile:	
+	string_eng=""
+	for keyword in myfile:
+		container.append(keyword.rstrip('\n'))
+	for i in range(len(container)):
+		string_eng += (container[i]+'* ')
+		count += 1
+		if(count==50):
+			string_cht = trans(string_eng)
+			chinese.extend(string_cht.split('*'))
+			string_eng=""
+			count=0
+			break
+	#for i in range(len(chinese)):
+		#string=chinese[i]
+		#print(string.decode("utf-8"))
+
+with open ("translated", "w") as myfile:
+	for i in range(len(chinese)):
+		myfile.write(container[i] + ' ')
+		myfile.write(chinese[i] + '\n')
